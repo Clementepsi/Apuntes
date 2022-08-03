296b458daefa8c1a380fe17948fdf8ff4ade5207
Clemente Flores <fmlclemente@hotmail.com>
2022-08-03 07:27:37 -0700

    Agregando los apuntes de Generation

---
+++ EjerciciosJS/Pratice.js
@@ -0,0 +1,11 @@
+function practice3(n) {
+    return (n % 7 == 0) && (n % 8 == 0) ? true : false;
+}
+
+function practice41(n) {
+    return (n > 10) ? true : false;
+}
+
+function practice42(n) {
+    return (n % 4 == 0) || (n % 9 == 0) ? true : false;
+}
---
+++ EjerciciosJS/variables.js
@@ -0,0 +1,39 @@
+// mejores prácticas
+// camelCase
+// Utilizar mayusculas en la mitad de nuestras variables
+// variableTriangulo, miNombre, miEdad
+// Palabras reservadas:
+// No usar palabras reservadas para declarar una variable
+// if, return, true, false, for, hile, let
+
+// Seguir un estandar personal
+// pueden tener contener numeros, letras,guion bajo, simbolo de dolar
+
+// Declaración de variables
+// Numero
+var edad = 24;
+var peso = 76;
+var astigmatismo = 3;
+// Typeboolean
+var ciego = true;
+var hambre = true;
+var ia = true;
+
+// Type String
+var nombre = " Clemente Flores";
+var dir = 'Aguascalientes';
+
+// Type Null
+var ganasDeVivir = null;
+var nineros = null;
+
+// Variables declaradas con un mismo valor
+var x,y,z =4;
+
+//Variables undefined
+var xx, yy, zz;
+
+// Variable que va signada de derecha a izquierda
+var uno = dos = tres =4;
+
+// Escribri en un sola linea de varibales undefined de nuestro carrito de compras
